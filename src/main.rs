a88e1ef827f95338538011ae41eac3361763ca33
KonsInator <konstantin@flammiger.info>
2021-10-26 19:50:58 +0200

    Vek3-file

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,7 @@
+mod vec3;
+
+use vec3::Vek3;
 use plotters::prelude::*;
-
-#[derive(Copy, Clone)]
-struct Vek3(f32, f32, f32);
-
-impl Vek3 {
-    fn alles(n: f32) -> Vek3 { Vek3(n, n, n) }
-
-    // elementweise Operationen
-    fn add_e(a: &Vek3, b: &Vek3) -> Vek3 { Vek3(a.0+b.0, a.1+b.1, a.2+b.2) }
-    fn sub_e(a: &Vek3, b: &Vek3) -> Vek3 { Vek3(a.0-b.0, a.1-b.1, a.2-b.2) }
-    fn mul_e(a: &Vek3, b: &Vek3) -> Vek3 { Vek3(a.0*b.0, a.1*b.1, a.2*b.2) }
-    fn add3_e(a: &Vek3, b: &Vek3, c: &Vek3) -> Vek3 { Vek3(a.0+b.0+c.0, a.1+b.1+c.1, a.2+b.2+c.2) }
-
-    // skalare Operationen
-    fn add_s(s: f32, vek: &Vek3) -> Vek3 { Vek3(s+vek.0, s+vek.1, s+vek.2) }
-    fn sub_s(s: f32, vek: &Vek3) -> Vek3 { Vek3(s-vek.0, s-vek.1, s-vek.2) }
-    fn mul_s(s: f32, vek: &Vek3) -> Vek3 { Vek3(s*vek.0, s*vek.1, s*vek.2) }
-
-    // nicht-statische Funktionen
-    fn set_min_max(&mut self, min: &Vek3, max: &Vek3) {
-        self.0 = f32::max(min.0, f32::min(max.0, self.0));
-        self.1 = f32::max(min.1, f32::min(max.1, self.1));
-        self.2 = f32::max(min.2, f32::min(max.2, self.2));
-    }
-}
 
 struct PIDParameter {
     k_p: f32,
